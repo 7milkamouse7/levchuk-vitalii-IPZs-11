54ebf2bb0fcd6517a34a80af8e826dfdf0b974f5
7milkamouse7 <7milkamouse7@gmail.com>
2023-12-20 10:40:42 +0200

    Final commit

--- js/script.js
+++ js/script.js
@@ -1,23 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-   
-    const body = document.getElementById('body');
-    const contactFormButton = this.getElementById('contactFormButton');
-    const contactFormButtonClose = this.getElementById('contactFormButtonClose');
-    const contactForm = this.getElementById('contactForm');
-    const formOverlay = this.getElementById('formOverlay');
-  
-   
-    contactFormButton.addEventListener('click', function () {
-        contactForm.classList.toggle('show');
-        formOverlay.classList.toggle('show');
-        body.classList.toggle('fixed');
-      });
-    contactFormButtonClose.addEventListener('click', function () {
-        contactForm.classList.remove('show');
-        formOverlay.classList.remove('show');
-        body.classList.remove('fixed');
-      });
-  });
+
   document.addEventListener('DOMContentLoaded', function () {
     const burgerButton = document.getElementById('burgerButton');
     const burgerButtonClose = document.getElementById('burgerButtonClose');
